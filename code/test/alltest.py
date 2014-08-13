e7668524e80eb9032b4ddf4bcd1532296671aefd
Christoph Weygand <christophweygand@googlemail.com>
2014-08-13 17:22:49 +0200

    refactored test dir, to work better with pycharm

--- code/test/alltest.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'Waigie'
-
-import unittest
-
-if __name__ == "__main__":
-    suite = unittest.TestLoader().discover('CCLang', pattern = "*_test.py")
-    unittest.TextTestRunner(verbosity=2).run(suite)
